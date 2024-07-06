6d22797537e49f97e175f1ea04b8079cac2370f9
me <lopezbeto305@gmail.com>
2024-07-05 22:55:11 -0400

    Need to fix testimonials carousel and fix layout

--- app.js
+++ app.js
@@ -9,3 +9,13 @@ document.getElementById('signup-form').addEventListener('submit', function(e) {
     // Add your sign up logic here
     alert('Signed up successfully!');
 });
+
+window.addEventListener('scroll', function() {
+    var navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
+
